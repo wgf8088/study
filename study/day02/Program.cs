a6bb579ea685eb2ffe5243dbe2b97c2deacfc1c2
supang <supang@supangseiko.com>
2018-04-18 17:45:40 +0800

    test

---
+++ study/day02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
